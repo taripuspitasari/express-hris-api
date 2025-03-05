30cd4f4d301fedc6d1905b5d0d4e26b5c0fb44df
Tari Puspita Sari <taripuspitasari12@gmail.com>
2025-03-05 10:11:00 +0700

    chore: add cors

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import cors from "cors";
 import {publicRouter} from "../route/public-api";
 import {errorMiddleware} from "../middleware/error-middleware";
 import {apiRouter} from "../route/api";
 
 export const web = express();
+web.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "x-token-api"],
+  })
+);
+
 web.use(express.json());
 
 web.use(publicRouter);
