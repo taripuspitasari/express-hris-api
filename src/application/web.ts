432689eeb74121db62892080c51b537d23850cf5
Tari Puspita Sari <taripuspitasari12@gmail.com>
2025-07-20 16:23:26 +0700

    chore: cors

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import {publicRouter} from "../routes/public-api";
 import {errorMiddleware} from "../middlewares/error-middleware";
 import {apiRouter} from "../routes/api";
 
 export const web = express();
+web.use(cors());
 web.use(express.json());
 web.use(publicRouter);
 web.use(apiRouter);
--- src/validations/user-validation.ts
+++ src/validations/user-validation.ts
@@ -3,18 +3,18 @@ import {z, ZodType} from "zod";
 export class UserValidation {
   static readonly REGISTER: ZodType = z.object({
     email: z.email().max(191),
-    password: z.string().max(191),
+    password: z.string().min(8).max(191),
     name: z.string().max(191),
   });
 
   static readonly LOGIN: ZodType = z.object({
     email: z.email().max(191),
-    password: z.string().max(191),
+    password: z.string().min(8).max(191),
   });
 
   static readonly UPDATE: ZodType = z.object({
     email: z.email().max(191).optional(),
-    password: z.string().max(191).optional(),
+    password: z.string().min(8).max(191).optional(),
     name: z.string().max(191).optional(),
   });
 }
